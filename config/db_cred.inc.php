8aaceba1ddf08e45e85bc298b98c0a15fd52b1f1
Brijesh Kumar <brij420@gmail.com>
2015-08-27 02:55:05 +0530

    Update db_cred.inc.php

--- config/db_cred.inc.php
+++ config/db_cred.inc.php
@@ -4,7 +4,7 @@
  * 	Database credential
  */
         const HOST = "localhost";
-        const DBNAME = "SID_DevDB_Test";  //Database Name
+        const DBNAME = "DevDB_Test";  //Database Name
         const USERNAME = "root";   //Database user name
         const PASSWORD = "root";   //database password
 ?>
